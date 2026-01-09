be8e8db345aba6424be6aef98faafd369c781961
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2026-01-09 19:16:11 +0530

    Update books to have read status

--- index.ts
+++ index.ts
@@ -1,3 +1,5 @@
+type ReadStatus = "unread" | "reading" | "read";
+
 const myLibrary: Book[] = [];
 
 class Book {
@@ -5,15 +7,33 @@ class Book {
   title: string;
   author: string;
   pages: number;
+  readStatus: ReadStatus;
 
-  constructor(title: string, author: string, pages: number) {
+  constructor(
+    title: string,
+    author: string,
+    pages: number,
+    readStatus: ReadStatus = "unread"
+  ) {
     this.id = crypto.randomUUID();
     this.title = title;
     this.author = author;
     this.pages = pages;
+    this.readStatus = readStatus;
   }
 }
 
-function addBookToLibrary(title: string, author: string, pages: number): void {
-  myLibrary.push(new Book(title, author, pages));
+function addBookToLibrary(
+  title: string,
+  author: string,
+  pages: number,
+  readStatus: ReadStatus = "unread"
+): void {
+  myLibrary.push(new Book(title, author, pages, readStatus));
 }
+
+addBookToLibrary("Don Quixote", "Miguel de Cervantes", 304);
+addBookToLibrary("A Tale of Two Cities", "Charles Dickens", 290);
+addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1206);
+addBookToLibrary("The Little Prince", "Antoine de Saint-Exupery", 96);
+addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 314);
