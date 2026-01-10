b5b897a24b269b134c4fc0fae250c7eefe7e98b2
Priyam Mohanty <66015282+pyctal@users.noreply.github.com>
2026-01-10 12:08:32 +0530

    Implement DOM rendering logic for library inventory
    Create function to dynamically populate the HTML table with data from
    the array.

--- index.ts
+++ index.ts
@@ -37,3 +37,25 @@ addBookToLibrary("A Tale of Two Cities", "Charles Dickens", 290);
 addBookToLibrary("The Lord of the Rings", "J.R.R. Tolkien", 1206);
 addBookToLibrary("The Little Prince", "Antoine de Saint-Exupery", 96);
 addBookToLibrary("Harry Potter and the Sorcerer's Stone", "J.K. Rowling", 314);
+
+const tableBody = document.querySelector("tbody");
+
+function renderLibrary(): void {
+  if (!tableBody) {
+    return;
+  }
+
+  tableBody.innerHTML = "";
+
+  myLibrary.forEach((book: Book) => {
+    const tableRow = document.createElement("tr");
+    tableRow.innerHTML = `<td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.pages}</td>
+    <td>${book.readStatus}</td>`;
+
+    tableBody.append(tableRow);
+  });
+}
+
+renderLibrary();
